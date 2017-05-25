61c0b01dba06cf65a44dc191d414112d420c93d1
lucasbarros <lucas-barros28@hotmail.com>
2017-05-24 23:27:58 -0300

    Trabalhando com validações

---
+++ Aula 11 - Trabalhando com validações/src/main/java/com/crud/financeiro/controller/EntidadesController.java
@@ -0,0 +1,43 @@
+package com.crud.financeiro.controller;
+
+import com.crud.financeiro.model.Entidade;
+import com.crud.financeiro.service.EntidadesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/entidades")
+public class EntidadesController {
+
+    private static final String INDEX = "entidade/CadastrarEntidade";
+
+    @Autowired
+    private EntidadesService entidadesService;
+
+    @RequestMapping(value = "/novo")
+    public String novo(){
+        return INDEX;
+    }
+
+    @RequestMapping(value = "/novo", method = RequestMethod.POST)
+    public String salvar(@Valid Entidade entidade, BindingResult result){
+
+        if (result.hasErrors()){
+
+            // TODO: Mostrar mensagem de erro
+            return novo();
+        }
+
+        System.out.println("Entidade: " + entidade.toString());
+        entidadesService.salvar(entidade);
+
+        return novo();
+    }
+
+}
---
+++ Aula 11 - Trabalhando com validações/src/main/java/com/crud/financeiro/model/Titulo.java
@@ -0,0 +1,133 @@
+package com.crud.financeiro.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "titulo")
+public class Titulo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @Column(name = "data_de_emissao")
+    private Date dataDeEmissao;
+
+    @Column(name = "data_de_validade")
+    private Date dataDeValidade;
+
+    @Column(name = "data_do_pagamento")
+    private Date dataDoPagamento;
+
+    private BigDecimal valor;
+
+    @Column(name = "valor_pago")
+    private BigDecimal valorPago;
+
+    @Column(name = "valor_original")
+    private BigDecimal valorOriginal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "entidadeId")
+    private Entidade entidade;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tipo_pagamento_id")
+    private TipoDePagamento tipoDePagamento;
+
+    @Enumerated(EnumType.STRING)
+    private Tipo tipo;
+
+    @Enumerated(EnumType.STRING)
+    private Situacao situacao;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public Entidade getEntidade() {
+        return entidade;
+    }
+
+    public void setEntidade(Entidade entidade) {
+        this.entidade = entidade;
+    }
+
+    public Date getDataDeEmissao() {
+        return dataDeEmissao;
+    }
+
+    public void setDataDeEmissao(Date dataDeEmissao) {
+        this.dataDeEmissao = dataDeEmissao;
+    }
+
+    public Date getDataDeValidade() {
+        return dataDeValidade;
+    }
+
+    public void setDataDeValidade(Date dataDeValidade) {
+        this.dataDeValidade = dataDeValidade;
+    }
+
+    public Date getDataDoPagamento() {
+        return dataDoPagamento;
+    }
+
+    public void setDataDoPagamento(Date dataDoPagamento) {
+        this.dataDoPagamento = dataDoPagamento;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public BigDecimal getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(BigDecimal valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public BigDecimal getValorOriginal() {
+        return valorOriginal;
+    }
+
+    public void setValorOriginal(BigDecimal valorOriginal) {
+        this.valorOriginal = valorOriginal;
+    }
+
+    public TipoDePagamento getTipoDePagamento() {
+        return tipoDePagamento;
+    }
+
+    public void setTipoDePagamento(TipoDePagamento tipoDePagamento) {
+        this.tipoDePagamento = tipoDePagamento;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public Situacao getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(Situacao situacao) {
+        this.situacao = situacao;
+    }
+}
