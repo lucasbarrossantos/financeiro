abd742becad522c8d6654e33cda7b4b9725d5368
lucasbarros <lucas-barros28@hotmail.com>
2017-05-27 13:31:56 -0300

    Edição de entidade

---
+++ Aula 14 - Edição de entidade/src/main/resources/static/vendor/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+// select
+$(document).ready(function() {
+    $('select').material_select();
+});
