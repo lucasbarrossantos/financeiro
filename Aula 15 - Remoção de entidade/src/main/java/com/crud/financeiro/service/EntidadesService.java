0a54a78ccd20c5fdd1da4aaa03e1192699cc74db
lucasbarros <lucas-barros28@hotmail.com>
2017-05-31 17:56:00 -0300

    Remoção de entidade

---
+++ Aula 15 - Remoção de entidade/src/main/java/com/crud/financeiro/service/EntidadesService.java
@@ -0,0 +1,21 @@
+package com.crud.financeiro.service;
+
+import com.crud.financeiro.model.Entidade;
+import com.crud.financeiro.repository.Entidades;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EntidadesService {
+
+    @Autowired
+    private Entidades entidades;
+
+    public void salvar(Entidade entidade){
+        entidades.save(entidade);
+    }
+
+    public void remover(Long codigo) {
+        entidades.delete(codigo);
+    }
+}
---
+++ Aula 15 - Remoção de entidade/src/main/resources/static/javascript/financeiro.js
@@ -0,0 +1,41 @@
+var Financeiro = Financeiro || {};
+
+Financeiro.DialogoDeRemocao = (function () {
+
+    function DialogoDeRemocao() {
+        this.modal = $('#modal-remover-entidade');
+        this.botaoRemover = $('.js-remover-entidade-btn');
+    }
+
+    DialogoDeRemocao.prototype.iniciar = function () {
+      this.botaoRemover.on('click', onModalShow.bind(this));
+    };
+
+    function onModalShow(evento){
+        evento.preventDefault();
+        var botao = $(evento.currentTarget);
+        var codigo = botao.data('codigo');
+        var nome = botao.data('nome');
+
+        var form = this.modal.find('form');
+        var action = form.data('url-remover');
+        if(!action.endsWith('/')){
+            action += '/';
+        }
+
+        form.attr('action', action + codigo);
+
+        this.modal.find('.modal-content span').
+        html('Tem certeza que deseja remover <strong> ' + nome + '</strong> ?');
+    }
+
+
+    return DialogoDeRemocao;
+
+}());
+
+$(function () {
+
+    var removerEntidade = new Financeiro.DialogoDeRemocao();
+    removerEntidade.iniciar();
+});
